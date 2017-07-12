d22151a89d5124b2c3dae686a15c83e111c85866
jmike <jmike@DESKTOP-54L68HD>
2017-07-12 10:30:16 -0700

    Initial commit

---
+++ ExtendedConsoleClass/ExtConsole.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendedConsoleClass
+{
+    class ExtConsole
+    {
+        private static string userInput;
+        private static int parsedNum;
+        private static double parsedDub;
+
+        // will parse a string for a double
+        public static int ParseInputInt(string someString)
+        {
+            int.TryParse(someString, out parsedNum);
+            return parsedNum;
+        }
+
+        // will prompt an input from the user and parse it for an int
+        public static int ParseInputInt()
+        {
+            userInput = Console.ReadLine();
+            int.TryParse(userInput, out parsedNum);
+            return parsedNum;
+        }
+
+        // will parse a string for a double
+        public static double ParseInputDouble(string someString)
+        {
+            double.TryParse(someString, out parsedDub);
+            return parsedDub;
+        }
+
+        // will prompt an input from the user and parse it for an double
+        public static double ParseInputDouble()
+        {
+            userInput = Console.ReadLine();
+            double.TryParse(userInput, out parsedDub);
+            return parsedDub;
+        }
+
+        //will take a string, print it to the console, and request input from the user and return the input
+        public static string RequestRecieve(string someText)
+        {
+            Console.WriteLine(someText);
+            userInput = Console.ReadLine();
+            Console.Clear();
+            return userInput;
+        }
+
+        //Will take a string and print it to the console. Will recieve user input and validate it.
+        //if the validation fails it will reprompt the user
+        public static int RequestRecieveInt(string someText, string inputValidationMessage)
+        {
+            while (true)
+            {
+                Console.WriteLine(someText);
+                userInput = Console.ReadLine();
+                Console.Clear();
+                if (int.TryParse(userInput, out parsedNum) == false)
+                {
+                    Console.WriteLine(inputValidationMessage);
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+            return parsedNum;
+        }
+    }
+}
+
---
+++ ExtendedConsoleClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedConsoleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demonstrations
+
+
+
+            //will return 10
+            string number = "5";
+            int newNumber = ExtConsole.ParseInputInt(number);
+            Console.WriteLine(newNumber + 5);
+
+            //will return 7.5
+            double newDouble = ExtConsole.ParseInputDouble(number);
+            Console.WriteLine(newDouble + 2.5);
+
+            // Will return the users name.
+            string message = "Enter your name: ";
+            string userName = ExtConsole.RequestRecieve(message);
+            Console.WriteLine(userName);
+
+            //Will return user input + 5 and do input validation
+            message = "Enter a number: ";
+            string validationMessage = "You must enter a whole number!";
+            newNumber = ExtConsole.RequestRecieveInt(message, validationMessage);
+            Console.WriteLine(newNumber + 5);
+
+        }
+    }
+}
